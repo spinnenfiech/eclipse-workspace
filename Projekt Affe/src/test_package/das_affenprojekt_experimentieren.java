9e8581d914521cc1a118778fb034353ba2a3ccf9
spinnenfiech <spinnenfiech@gmail.com>
2017-11-28 19:00:55 +0100

    ?
    ??

--- Projekt Affe/src/test_package/das_affenprojekt_experimentieren.java
+++ Projekt Affe/src/test_package/das_affenprojekt_experimentieren.java
@@ -7,45 +7,66 @@ public class das_affenprojekt_experimentieren
 
 	public static void main(String[] args)
 	{
-		String nstring = new String();
+		String nstring = new String(RandomTextausgabe(30));
+		String nstring2 = new String(RandomTextausgabe2(30));
+		System.out.println(nstring);
 		StringBuilder nstringb = new StringBuilder(30);
 		//nstringb.setCharAt(1, (char) ('a' + 26*Math.random()));
-		
-		for (int i = 0; i < nstring.length(); i++)	//gibt jeden einzelnen Buchstabe pro Zeile aus
+		int f = 0;
+		while(true)
 		{
+			nstring.replaceAll(nstring, nstring2);
+			nstring2.replaceAll(nstring2, nstring);
+			f++;
+			//System.out.println(f);
+		}
+		
+		//for (int i = 0; i < nstring.length(); i++)	//gibt jeden einzelnen Buchstabe pro Zeile aus
+		//{
 			//System.out.println(nstring.charAt(i));
-		}
+		//}
 		
 		
-		for (int i = 0; i < nstring.length(); i++)	//baut den String treppenartig auf
-		{
+		//for (int i = 0; i < nstring.length(); i++)	//baut den String treppenartig auf
+		//{
 			//System.out.println(nstring.substring(i));
-		}
+		//}
 		//nRandom test = new nRandom();
-		
 		//System.out.println(nRandom);
 		
 		
 	}//main Ende
 	
-	public String RandomTextausgabe() 
+	public static String RandomTextausgabe(int j) 
 	{
 		char[] zahl = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-		String text = new String();
-		//String hoden = text;
-		
-		System.out.println(zahl);
-		
-		text = "";
+		String text = "";
 		Random r2 = new Random();
-		
-		for(int i = 0; i < text.length(); i++)
-		{
-			text = text + zahl[r2.nextInt(zahl.length)];
-		}
-		return text;
+			for(int i = 0; i < zahl.length; i++)
+			{
+				text = text + zahl[r2.nextInt(zahl.length)];
+			}
+			System.out.println(text);
+			System.out.println(text.length());
+			return text;
 	}
 	
+	public static String RandomTextausgabe2(int j) 
+	{
+		char[] zahl = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		String text = "";
+		Random r2 = new Random();
+		while(true)
+		{
+			for(int i = 0; i < zahl.length; i++)
+			{
+				text = text + zahl[r2.nextInt(zahl.length)];
+			}
+			System.out.println(text);
+			System.out.println(text.length());
+			return text;
+		}
+	}
 	
 	public static StringBuilder nRandom;
 	{
