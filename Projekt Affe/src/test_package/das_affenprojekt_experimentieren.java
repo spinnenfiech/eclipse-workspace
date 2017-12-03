1e5da2d6d7cca59b2ceb2a59c45166aa260340a0
spinnenfiech <spinnenfiech@gmail.com>
2017-12-03 04:44:22 +0100

    4

--- Projekt Affe/src/test_package/das_affenprojekt_experimentieren.java
+++ Projekt Affe/src/test_package/das_affenprojekt_experimentieren.java
@@ -7,53 +7,21 @@ public class das_affenprojekt_experimentieren
 
 	public static void main(String[] args)
 	{
-		String nstring = new String(RandomTextausgabe());
-		System.out.println(nstring);
-		//nstringb.setCharAt(1, (char) ('a' + 26*Math.random()));
-
-		//for (int i = 0; i < nstring.length(); i++)	//gibt jeden einzelnen Buchstabe pro Zeile aus
-		//{
-			//System.out.println(nstring.charAt(i));
-		//}
-		
-		
-		//for (int i = 0; i < nstring.length(); i++)	//baut den String treppenartig auf
-		//{
-			//System.out.println(nstring.substring(i));
-		//}
-		//nRandom test = new nRandom();
-		//System.out.println(nRandom);
-		
-		
-	}//main Ende
+		while(true) //Die Schleife, um das unten angegebene CharArray unendlich oft herzugeben
+		{
+			System.out.println(RandomTextausgabe());
+		}
+	} //Main Ende
 	
-	public static char[] RandomTextausgabe() 
+	public static char[] RandomTextausgabe() //Die Methode, das zufällige CharArray zu bauen
 	{
-		char[] zahl = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-		char[] text = new char[30];
-		Random r2 = new Random();
-		for(int i = 0; i < text.length; i++)
+		char[] zahl = "abcdefghijklmnopqrstuvwxyz".toCharArray(); //Das Alphabet als Pool für das Array
+		char[] text = new char[30]; //Die länge des Arrays
+		Random r2 = new Random(); //Der Zufalls-Affe
+		for(int i = 0; i < text.length; i++) //Die Schleife, um das Array mit zufälligen Charakteren zu füllen
 		{
 			text[i] = zahl[r2.nextInt(zahl.length)];
 		}
 		return text;
-	}
-	
-	public static StringBuilder nRandom;
-	{
-		StringBuilder nRandomFill = new StringBuilder();
-		//nRandomFill.setCharAt(1, (char) ('a' + 26*Math.random()));
-		String nStringIrgendwas = nRandomFill.toString();
-		
-		nRandom.setLength(30);
-		nRandom.replace(1, 30, nStringIrgendwas);
-		
-		String nstring = new String("das ist ein Test");
-		
-		for (int i = 0; i < nstring.length(); i++)	//gibt jeden einzelnen Buchstabe pro Zeile aus
-		{
-			System.out.println(nstring.charAt(i));
-		}
-		
 	}
 }
