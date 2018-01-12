5372ed4df2f289da322666bb153f09cca8e5b86d
Der schwarze Mann <spinnenfiech@gmail.com>
2018-01-12 22:15:19 +0100

    commiz

--- Projekt Affe/src/test_package/das_affenprojekt_experimentieren.java
+++ Projekt Affe/src/test_package/das_affenprojekt_experimentieren.java
@@ -6,18 +6,19 @@ import javafx.scene.control.Label;
 
 public class das_affenprojekt_experimentieren //extends Label
 {
-	public String RandomTextausgabe() throws InterruptedException //Die Methode, das zufällige CharArray zu bauen
-	{
-		hud inputFieldLänge = new hud();
-		
+	static hud inputFieldLänge = new hud();
+	
+	public static String RandomTextausgabe() throws InterruptedException //Die Methode, das zufällige CharArray zu bauen
+	{		
 		char[] zahl = "abcdefghijklmnopqrstuvwxyz".toCharArray(); //Das Alphabet als Pool für das Array
 		
 		String text = new String();
 		
 		Random r2 = new Random(); //Der Zufalls-Affe
 		
-		for(int i = 0; i < inputFieldLänge.getMeineZahl(); i++) //Die Schleife, um das Array mit zufälligen Charakteren zu füllen
+		for(int i = 0; i < 4; i++) //Die Schleife, um das Array mit zufälligen Charakteren zu füllen
 		{
+			System.out.println("Das ist 'inputFieldLänge': " + inputFieldLänge.getMeineZahl());
 			text = text + zahl[r2.nextInt(zahl.length)];
 		}
 		return text;
